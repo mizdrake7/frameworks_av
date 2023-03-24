b7f42c0fa25cb2362490ca9900548c9eb80ffe1d
Akash Srivastava <akashniki@gmail.com>
2023-03-24 16:58:22 +0100

    Revert "DO NOT MERGE libcameraservice: Provide flag for overriding camera output to portrait."
    This reverts commit 3560b7e9373b54f8662d3f3f0df9c2c3b35a4c33.
    
    Change-Id: I42b0508e245b35da40d25960798b2654764b0d6e
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- services/camera/libcameraservice/common/hidl/HidlProviderInfo.cpp
+++ services/camera/libcameraservice/common/hidl/HidlProviderInfo.cpp
@@ -912,8 +912,7 @@ status_t HidlProviderInfo::convertToHALStreamCombinationAndCameraIdsLocked(
         bool overrideForPerfClass =
                 SessionConfigurationUtils::targetPerfClassPrimaryCamera(
                         perfClassPrimaryCameraIds, cameraId, targetSdkVersion);
-        res = mManager->getCameraCharacteristicsLocked(cameraId, overrideForPerfClass, &deviceInfo,
-                /*overrideToPortrait*/true);
+        res = mManager->getCameraCharacteristicsLocked(cameraId, overrideForPerfClass, &deviceInfo);
         if (res != OK) {
             return res;
         }
@@ -921,7 +920,7 @@ status_t HidlProviderInfo::convertToHALStreamCombinationAndCameraIdsLocked(
                 [this](const String8 &id, bool overrideForPerfClass) {
                     CameraMetadata physicalDeviceInfo;
                     mManager->getCameraCharacteristicsLocked(id.string(), overrideForPerfClass,
-                            &physicalDeviceInfo, /*overrideToPortrait*/true);
+                                                   &physicalDeviceInfo);
                     return physicalDeviceInfo;
                 };
         std::vector<std::string> physicalCameraIds;

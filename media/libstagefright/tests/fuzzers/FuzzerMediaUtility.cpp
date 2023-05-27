littleGnAl/mirror_platform_frameworks_av

libstagefright_writer_fuzzer : Bug Fix
Initialized 'trackMime' using FuzzedDataProvider

Test: ./libstagefright_writer_fuzzer clusterfuzz-testcase-minimized-libstagefright_writer_fuzzer-5675078924238848
Bug: 190994150

Change-Id: Ie0c85b6afd21077d1c317ed57607fb1dcfacfabc

media/libstagefright/tests/fuzzers/FuzzerMediaUtility.cpp
@@ -65,8 +65,8 @@ sp<MediaSource> genMediaSource(FuzzedDataProvider *dataProvider, uint16_t maxMed
     for (size_t i = 0; i < extractor->countTracks(); ++i) {
         sp<MetaData> meta = extractor->getTrackMetaData(i);
 
-        const char *trackMime;
-        if (!strcasecmp(mime.c_str(), trackMime)) {
+        std::string trackMime = dataProvider->PickValueInArray(kSupportedMimeTypes);
+        if (!strcasecmp(mime.c_str(), trackMime.c_str())) {
             sp<IMediaSource> track = extractor->getTrack(i);
             if (track == NULL) {
                 return NULL;

media/libstagefright/tests/fuzzers/FuzzerMediaUtility.h
@@ -42,6 +42,51 @@ enum StandardWriters {
     kMaxValue = MPEG2TS,
 };
 
+static std::string kSupportedMimeTypes[] = {"audio/3gpp",
+                                            "audio/amr-wb",
+                                            "audio/vorbis",
+                                            "audio/opus",
+                                            "audio/mp4a-latm",
+                                            "audio/mpeg",
+                                            "audio/mpeg-L1",
+                                            "audio/mpeg-L2",
+                                            "audio/midi",
+                                            "audio/qcelp",
+                                            "audio/g711-alaw",
+                                            "audio/g711-mlaw",
+                                            "audio/flac",
+                                            "audio/aac-adts",
+                                            "audio/gsm",
+                                            "audio/ac3",
+                                            "audio/eac3",
+                                            "audio/eac3-joc",
+                                            "audio/ac4",
+                                            "audio/scrambled",
+                                            "audio/alac",
+                                            "audio/x-ms-wma",
+                                            "audio/x-adpcm-ms",
+                                            "audio/x-adpcm-dvi-ima",
+                                            "video/avc",
+                                            "video/hevc",
+                                            "video/mp4v-es",
+                                            "video/3gpp",
+                                            "video/x-vnd.on2.vp8",
+                                            "video/x-vnd.on2.vp9",
+                                            "video/av01",
+                                            "video/mpeg2",
+                                            "video/dolby-vision",
+                                            "video/scrambled",
+                                            "video/divx",
+                                            "video/divx3",
+                                            "video/xvid",
+                                            "video/x-motion-jpeg",
+                                            "text/3gpp-tt",
+                                            "application/x-subrip",
+                                            "text/vtt",
+                                            "text/cea-608",
+                                            "text/cea-708",
+                                            "application/x-id3v4"};
+
 std::string genMimeType(FuzzedDataProvider *dataProvider);
 sp<IMediaExtractor> genMediaExtractor(FuzzedDataProvider *dataProvider, uint16_t dataAmount);
 sp<MediaSource> genMediaSource(FuzzedDataProvider *dataProvider, uint16_t maxMediaBlobSize);

mizdrake7/frameworks_av

Signed-off-by: hungphan2001 <phandinhhungvp2001@gmail.com>
Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

media/libstagefright/tests/fuzzers/FuzzerMediaUtility.cpp
@@ -66,8 +66,8 @@ sp<MediaSource> genMediaSource(FuzzedDataProvider *dataProvider, uint16_t maxMed
     for (size_t i = 0; i < extractor->countTracks(); ++i) {
         sp<MetaData> meta = extractor->getTrackMetaData(i);
 
-        const char *trackMime;
-        if (!strcasecmp(mime.c_str(), trackMime)) {
+        std::string trackMime = dataProvider->PickValueInArray(kTestedMimeTypes);
+        if (!strcasecmp(mime.c_str(), trackMime.c_str())) {
             sp<IMediaSource> track = extractor->getTrack(i);
             if (track == NULL) {
                 return NULL;

media/libstagefright/tests/fuzzers/FuzzerMediaUtility.h
@@ -42,6 +42,51 @@ enum StandardWriters {
     kMaxValue = MPEG2TS,
 };
 
+static std::string kTestedMimeTypes[] = {"audio/3gpp",
+                                         "audio/amr-wb",
+                                         "audio/vorbis",
+                                         "audio/opus",
+                                         "audio/mp4a-latm",
+                                         "audio/mpeg",
+                                         "audio/mpeg-L1",
+                                         "audio/mpeg-L2",
+                                         "audio/midi",
+                                         "audio/qcelp",
+                                         "audio/g711-alaw",
+                                         "audio/g711-mlaw",
+                                         "audio/flac",
+                                         "audio/aac-adts",
+                                         "audio/gsm",
+                                         "audio/ac3",
+                                         "audio/eac3",
+                                         "audio/eac3-joc",
+                                         "audio/ac4",
+                                         "audio/scrambled",
+                                         "audio/alac",
+                                         "audio/x-ms-wma",
+                                         "audio/x-adpcm-ms",
+                                         "audio/x-adpcm-dvi-ima",
+                                         "video/avc",
+                                         "video/hevc",
+                                         "video/mp4v-es",
+                                         "video/3gpp",
+                                         "video/x-vnd.on2.vp8",
+                                         "video/x-vnd.on2.vp9",
+                                         "video/av01",
+                                         "video/mpeg2",
+                                         "video/dolby-vision",
+                                         "video/scrambled",
+                                         "video/divx",
+                                         "video/divx3",
+                                         "video/xvid",
+                                         "video/x-motion-jpeg",
+                                         "text/3gpp-tt",
+                                         "application/x-subrip",
+                                         "text/vtt",
+                                         "text/cea-608",
+                                         "text/cea-708",
+                                         "application/x-id3v4"};
+
 std::string genMimeType(FuzzedDataProvider *dataProvider);
 sp<IMediaExtractor> genMediaExtractor(FuzzedDataProvider *dataProvider, uint16_t dataAmount);
 sp<MediaSource> genMediaSource(FuzzedDataProvider *dataProvider, uint16_t maxMediaBlobSize);